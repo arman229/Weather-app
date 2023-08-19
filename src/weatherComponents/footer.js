65b53b421723f58ee50a7d4f95571217283f2a29
Arman229 <armanashraf015@gmail.com>
2023-08-19 15:26:49 +0500

    add the link of the portfolio website

--- src/weatherComponents/footer.js
+++ src/weatherComponents/footer.js
@@ -2,6 +2,16 @@ import React from "react";
 import { Typography } from '@mui/material';
 import { Container, Grid, Link   } from '@mui/material';
 const Footer = () => {
+
+
+    const styles = {
+        color:"white",
+
+        fontFamily: "Open Sans",
+        fontStyle: 'italic',
+
+        src: "local('Open Sans Bold Italic'), local('OpenSans-BoldItalic'), url(http://themes.googleusercontent.com/static/fonts/opensans/v8/PRmiXeptR36kaC0GEAetxjqR_3kx9_hJXbbyU8S6IN0.woff) format('woff')",
+    };
     return (
         <footer style={{ backgroundColor: 'gray', color:"white", padding: '20px 0' }}>
             <Container maxWidth="lg">
@@ -14,7 +24,7 @@ const Footer = () => {
                     <Grid item sm={6} xs={12}>
                         <Typography variant="body2" textAlign={{ xs: 'center', sm: 'right'  }} >
                             Designed and developed by{' '}
-                            <Link   href="https://www.portfolioarman" target="_blank" rel="noopener" style={{color:"white",fontSize:"1.2em" }}>
+                            <Link   href="https://portfolio-arman.netlify.app"  style={styles}    target="_blank" rel="noopener" >
                                 ARMAN
                             </Link>
                         </Typography>
